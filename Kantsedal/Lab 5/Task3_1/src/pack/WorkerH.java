9d1dc6db244fff7f3a860f5eba7b078fd6b3dfb5
GeorgKantsedal <33697901+GeorgKantsedal@users.noreply.github.com>
2017-11-27 10:34:45 +0200

    Add files via upload

---
+++ Kantsedal/Lab 5/Task3_1/src/pack/WorkerH.java
@@ -0,0 +1,9 @@
+package pack;
+
+
+public class WorkerH extends Worker {
+    public WorkerH (double zar, String name) {super (zar, name);}
+    public  double zarout (){
+        return (20.8*8*zar);
+    }
+}
